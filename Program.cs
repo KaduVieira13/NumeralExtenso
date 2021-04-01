9edf8ff8ae9ff46eba5e06efa9e6caa7c7a4185d
KaduVieira13 <carloseduardo_alencar@outlook.com>
2021-04-01 16:04:06 -0300

    Programa de exibição de numeral extenso finalizado

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Humanizer;
+
+namespace NumeralExtenso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" --- Numeral por Extenso --- ");
+            Console.Write("Digite o numero desejado: ");
+
+            string numeroDigitado = Console.ReadLine();
+            int numeroNovo = Convert.ToInt32(numeroDigitado);
+            string exibirExtenso = numeroNovo.ToWords();
+            string exibirRomano = numeroNovo.ToRoman();
+
+            Console.WriteLine($"O numero digitado é: {exibirExtenso}");
+            Console.WriteLine($"O numero digitado em algarismos romanos é: {exibirRomano} ");
+
+            
+
+
+        }
+    }
+}
